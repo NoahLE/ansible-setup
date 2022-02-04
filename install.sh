4453635bb3362723d07812fa74f36f3f93337876
NoahLE <noah@noahle.com>
2022-02-03 23:47:38 -0600

    Fixing bugs

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
-sudo apt update && 
-sudo apt upgrade -y &&
-sudo apt autoremove &&
-sudo apt install -y software-properties-common git ansible && 
-sudo add-apt-repository -y --update ppa:ansible/ansible &&
-sudo ansible-galaxy collection install community.general &&
-echo "From this directory, run the following command: `ansible-playbook -i inventory setup.yml -K`"
+echo ">>> updating"
+sudo apt update 
+sudo apt upgrade -y
+sudo apt autoremove
+
+echo ">>> installing deps"
+sudo apt install -y software-properties-common git ansible
+
+echo ">>> installing ansible"
+sudo add-apt-repository -y --update ppa:ansible/ansible
+sudo ansible-galaxy collection install community.general
+
+echo ">>> From this directory, run the following command: ansible-playbook -i inventory setup.yml -K"
